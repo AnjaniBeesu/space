4ad6e8e2bee1bb5c7fce0fdf285156c11555246a
ANJANI  BEESU <anjanibeesu@gmail.com>
2026-09-24 21:54:56 +0530

    add ISS tracker preloader

---
+++ app/components/preloader.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Preloader() {
+  return (
+    <div className="preloader">
+      <div className="crack crack1"></div>
+      <div className="crack crack2"></div>
+      <div className="crack crack3"></div>
+      <div className="crack crack4"></div>
+      <div className="crack crack5"></div>
+    </div>
+  );
+}
