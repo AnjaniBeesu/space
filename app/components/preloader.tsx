c58afae6ffcd43abb2090ca30738f15f8b7872e9
ANJANI  BEESU <anjanibeesu@gmail.com>
2026-09-24 22:35:03 +0530

    position loading label beneath preloader star

--- app/components/preloader.tsx
+++ app/components/preloader.tsx
@@ -8,7 +8,22 @@ export default function Preloader() {
       <div className="crack crack3"></div>
       <div className="crack crack4"></div>
       <div className="crack crack5"></div>
-      <span className="preloader-loading">loading</span>
+      <span
+        className="preloader-loading"
+        style={{
+          position: "absolute",
+          top: "calc(50% + 48px)",
+          left: "50%",
+          transform: "translateX(-50%)",
+          color: "#fef3fc",
+          fontFamily: "'DM Mono', monospace",
+          fontSize: "10px",
+          letterSpacing: ".16em",
+          textTransform: "lowercase",
+        }}
+      >
+        loading
+      </span>
     </div>
   );
 }
