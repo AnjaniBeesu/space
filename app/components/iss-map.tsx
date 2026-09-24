ba0b88fecaca69a25c6dba4133332de24c631f0b
ANJANI  BEESU <anjanibeesu@gmail.com>
2026-09-24 19:11:25 +0530

    feat: switch ISS map to satellite imagery

--- app/components/iss-map.tsx
+++ app/components/iss-map.tsx
@@ -17,12 +17,18 @@ export default function IssMap({ position, trail }: { position: Position; trail:
   return (
     <MapContainer center={[position.latitude, position.longitude]} zoom={2} minZoom={2} maxZoom={6} scrollWheelZoom className="iss-map">
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.esri.com/">Esri</a> contributors'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        maxZoom={19}
       />
-      {trail.length > 1 && <Polyline positions={trail.map((p) => [p.latitude, p.longitude] as [number, number])} pathOptions={{ color: "#ff6bd6", weight: 3, opacity: 0.7 }} />}
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        opacity={0.18}
+      />
+      {trail.length > 1 && <Polyline positions={trail.map((p) => [p.latitude, p.longitude] as [number, number])} pathOptions={{ color: "#ff6bd6", weight: 3, opacity: 0.85 }} />}
       <CircleMarker center={[position.latitude, position.longitude]} radius={9} pathOptions={{ color: "#fff", weight: 3, fillColor: "#ff6bd6", fillOpacity: 1 }} />
-      <CircleMarker center={[position.latitude, position.longitude]} radius={18} pathOptions={{ color: "#ff6bd6", weight: 1, fillOpacity: 0, opacity: 0.35 }} />
+      <CircleMarker center={[position.latitude, position.longitude]} radius={18} pathOptions={{ color: "#ff6bd6", weight: 1, fillOpacity: 0, opacity: 0.45 }} />
       <Recenter position={position} />
     </MapContainer>
   );
