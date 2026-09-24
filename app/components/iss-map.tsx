11d62d626e9477a2d6c77baf91dc21295dc2b060
ANJANI  BEESU <anjanibeesu@gmail.com>
2026-09-24 18:50:47 +0530

    feat: add interactive ISS map

---
+++ app/components/iss-map.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { CircleMarker, MapContainer, Polyline, TileLayer, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+type Position = { latitude: number; longitude: number };
+
+function Recenter({ position }: { position: Position }) {
+  const map = useMap();
+  useEffect(() => {
+    map.panTo([position.latitude, position.longitude], { animate: true, duration: 0.8 });
+  }, [map, position.latitude, position.longitude]);
+  return null;
+}
+
+export default function IssMap({ position, trail }: { position: Position; trail: Position[] }) {
+  return (
+    <MapContainer center={[position.latitude, position.longitude]} zoom={2} minZoom={2} maxZoom={6} scrollWheelZoom className="iss-map">
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {trail.length > 1 && <Polyline positions={trail.map((p) => [p.latitude, p.longitude] as [number, number])} pathOptions={{ color: "#ff6bd6", weight: 3, opacity: 0.7 }} />}
+      <CircleMarker center={[position.latitude, position.longitude]} radius={9} pathOptions={{ color: "#fff", weight: 3, fillColor: "#ff6bd6", fillOpacity: 1 }} />
+      <CircleMarker center={[position.latitude, position.longitude]} radius={18} pathOptions={{ color: "#ff6bd6", weight: 1, fillOpacity: 0, opacity: 0.35 }} />
+      <Recenter position={position} />
+    </MapContainer>
+  );
+}
